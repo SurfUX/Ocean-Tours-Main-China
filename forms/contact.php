ebb9ebeeb9b10db1f85a669109230adbe61615c0
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-02-24 05:32:50 +0530

    Email

--- forms/contact.php
+++ forms/contact.php
@@ -1,42 +1,29 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$to = "gihan@surfux.com";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$name    = $_POST['name'] ?? '';
+$email   = $_POST['email'] ?? '';
+$subject = $_POST['subject'] ?? 'New Contact Form Message';
+$message = $_POST['message'] ?? '';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+if (empty($name) || empty($email) || empty($message)) {
+    echo "Please fill all required fields.";
+    exit;
+}
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$headers  = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$body  = "New Contact Form Submission\n\n";
+$body .= "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Subject: $subject\n\n";
+$body .= "Message:\n$message\n";
 
-  echo $contact->send();
-?>
+if (mail($to, $subject, $body, $headers)) {
+    echo "OK";
+} else {
+    echo "Failed to send message. Please try again later.";
+}
